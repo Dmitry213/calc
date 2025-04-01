270b16cc94d2d35ea07e6ae2d59ad40c001370da
Dmitry213 <chetkarevdima@gmail.com>
2025-04-01 22:22:51 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
-document.addEventListener('DOMContentLoaded', function(){
-    console.log('test');
+ document.addEventListener('DOMContentLoaded', function(){
     var form = document.querySelector('.calc')
-    form.addEventListener('sumbit', function(e){
+    form.addEventListener('submit', function(e){
         e.preventDefault()
     })
     var res_field = document.querySelector('.calc__result-field')
 
     var btn_num = document.querySelectorAll('.js--btn-add-res')
     var btn_reset = document.querySelector('.js--btn-reset')
-    var btn_calc = document.querySelector('.js--btn-calc')
+    var btn_calc = document.querySelector('.js--btn-add-calc')
     for(i=0; i < btn_num.length; i++){
         btn_num[i].addEventListener('click', function(e){
             e.preventDefault()
-            res_field.value = this.innerHTML;
+            res_field.value += this.innerHTML;
         })
     }
     btn_reset.addEventListener('click', function(e){
         e.preventDefault()
-        res_field.value += '';
+        res_field.value = '';
     })
 
     btn_calc.addEventListener('click', function(e){
         e.preventDefault()
         res_field.value = eval(res_field.value);
     })
-})
+  })
